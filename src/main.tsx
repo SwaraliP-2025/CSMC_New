9ad6c6735bcf1f9f0b0838e67bd7d812b34fb7c8
SwaraliP <swarali.pathrikar@consultmipl.com>
2026-09-04 13:18:53 +0530

    font d

--- src/main.tsx
+++ src/main.tsx
@@ -3,4 +3,14 @@ import App from "./App.tsx";
 import "./styles/devanagari-fonts.css";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const rootEl = document.getElementById("root")!;
+
+async function boot() {
+  const loads = ["400", "500", "600", "700"].map((w) =>
+    document.fonts.load(`${w} 16px "Noto Sans Devanagari"`).catch(() => undefined),
+  );
+  await Promise.all([...loads, document.fonts.ready]);
+  createRoot(rootEl).render(<App />);
+}
+
+boot();
